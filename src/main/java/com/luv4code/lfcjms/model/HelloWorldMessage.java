da320f54f9b6663f165386b9d2532d9dfb95cf52
luv4code-lfc <luv4code.lc@gmail.com>
2021-12-08 16:14:48 +0530

    Adding hello world message pojo

---
+++ src/main/java/com/luv4code/lfcjms/model/HelloWorldMessage.java
@@ -0,0 +1,21 @@
+package com.luv4code.lfcjms.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class HelloWorldMessage implements Serializable {
+
+    static final long serialVersionUID = 1L;
+
+    private UUID id;
+    private String message;
+}
