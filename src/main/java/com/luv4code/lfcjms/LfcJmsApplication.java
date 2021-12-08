cf8693811309945242afaaf774bec308d72fecd2
luv4code-lfc <luv4code.lc@gmail.com>
2021-12-08 16:06:25 +0530

    Initial commit

---
+++ src/main/java/com/luv4code/lfcjms/LfcJmsApplication.java
@@ -0,0 +1,13 @@
+package com.luv4code.lfcjms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LfcJmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LfcJmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/luv4code/lfcjms/LfcJmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luv4code.lfcjms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LfcJmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
